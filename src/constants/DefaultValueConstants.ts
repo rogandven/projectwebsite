ea7717d8793880367f14bb30c7e56c7982b805c1
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-03-29 17:10:36 -0300

    WIP

--- src/constants/DefaultValueConstants.ts
+++ src/constants/DefaultValueConstants.ts
@@ -1,9 +1,16 @@
 export const DEFAULT_APPLICATION_NAME : string = "daisyUI";
 export const DEFAULT_SLOGAN : string = "Providing reliable tech since 1992";
 export const DEFAULT_LINK : string = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-export const HOME_DIRECTORY : string = "./";
+export const HOME_DIRECTORY : string = "/";
 export const DEFAULT_TITLE : string = "Hello there";
 export const DEFAULT_DESCRIPTION : string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 export const DEFAULT_BUTTON_LABEL : string = "Get Started";
 export const DEFAULT_IMAGE_LINK : string = "https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp";
 export const DEFAULT_ICON_NAME : string = "mdi:block";
+
+export const DEFAULT_POST_TITLE : string = "Default Title";
+export const DEFAULT_POST_DATE : string = "1970-01-01";
+export const DEFAULT_POST_AUTHOR : string = "Default Author";
+export const DEFAULT_POST_IMAGE : string = DEFAULT_IMAGE_LINK;
+export const DEFAULT_POST_IMAGE_ALT : string = "Default Image";
+export const DEFAULT_POST_DESCRIPTION : string = "Default Description";
--- src/utils/GeneralUtils.ts
+++ src/utils/GeneralUtils.ts
@@ -1,5 +1,42 @@
-import { DEFAULT_APPLICATION_NAME } from "../constants/DefaultValueConstants.ts"
+import { DEFAULT_APPLICATION_NAME, DEFAULT_LINK, DEFAULT_POST_AUTHOR, DEFAULT_POST_DATE, DEFAULT_POST_DESCRIPTION, DEFAULT_POST_IMAGE, DEFAULT_POST_IMAGE_ALT, DEFAULT_POST_TITLE } from "../constants/DefaultValueConstants.ts"
 
 export const titleGenerator = (title: string) => {
     return String(title + " | " + DEFAULT_APPLICATION_NAME);
+}
+
+export const getPosts = () : any[] => {
+    return Object.values(import.meta.glob('../pages/blog/posts/*.md', { eager: true }));
+} 
+
+const isPost = (post: any) : boolean => {
+    if (!post || typeof(post) !== "object" || Array.isArray(post) || !(post.frontmatter)) {
+        return false;
+    }
+    return true;
+}
+
+export const getPostInfo = (post: any) => {
+    const returnValue = {
+        title: DEFAULT_POST_TITLE,
+        date: DEFAULT_POST_DATE,
+        author: DEFAULT_POST_AUTHOR,
+        image: DEFAULT_POST_IMAGE,
+        image_alt: DEFAULT_POST_IMAGE_ALT,
+        description: DEFAULT_POST_DESCRIPTION,
+        url: DEFAULT_LINK,
+    };
+
+    if (!isPost(post)) {
+        return returnValue;
+    }
+
+    returnValue.title = String(post?.title || post?.frontmatter?.title || returnValue.title);
+    returnValue.date = String(post?.date || post?.frontmatter?.date || returnValue.date);
+    returnValue.author = String(post?.author || post?.frontmatter?.author || returnValue.author);
+    returnValue.image = String(post?.image?.src || post?.frontmatter?.image?.src || returnValue.image);
+    returnValue.image_alt = String(post?.image?.alt || post?.frontmatter?.image?.alt || returnValue.image_alt);
+    returnValue.description = String(post?.description || post?.frontmatter?.description || returnValue.description);
+    returnValue.url = String(post?.url || post?.frontmatter?.url || returnValue.url);
+
+    return returnValue;
 }
