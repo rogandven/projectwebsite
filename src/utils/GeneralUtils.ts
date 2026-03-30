7777c8a5f053ca73e6f1ae293efc52cb3bb72ddd
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-03-29 21:40:30 -0300

    Implemented PostInfo in GeneralUtils

--- src/utils/GeneralUtils.ts
+++ src/utils/GeneralUtils.ts
@@ -1,3 +1,4 @@
+import PostInfo from "../classes/PostInfo.ts";
 import { DEFAULT_APPLICATION_NAME, DEFAULT_LINK, DEFAULT_LOCALE, DEFAULT_POST_AUTHOR, DEFAULT_POST_DATE, DEFAULT_POST_DESCRIPTION, DEFAULT_POST_IMAGE, DEFAULT_POST_IMAGE_ALT, DEFAULT_POST_TITLE } from "../constants/DefaultValueConstants.ts"
 import { MAX_PREVIEW_LENGTH } from "../constants/TailwindConstants.ts";
 
@@ -16,16 +17,8 @@ const isPost = (post: any) : boolean => {
     return true;
 }
 
-export const getPostInfo = (post: any) => {
-    const returnValue = {
-        title: DEFAULT_POST_TITLE,
-        date: DEFAULT_POST_DATE,
-        author: DEFAULT_POST_AUTHOR,
-        image: DEFAULT_POST_IMAGE,
-        image_alt: DEFAULT_POST_IMAGE_ALT,
-        description: DEFAULT_POST_DESCRIPTION,
-        url: DEFAULT_LINK,
-    };
+export const getPostInfo = (post: any): PostInfo => {
+    const returnValue = new PostInfo();
 
     if (!isPost(post)) {
         return returnValue;
@@ -35,9 +28,9 @@ export const getPostInfo = (post: any) => {
     returnValue.date = String(post?.date || post?.frontmatter?.date || returnValue.date);
     returnValue.author = String(post?.author || post?.frontmatter?.author || returnValue.author);
     returnValue.image = String(post?.image?.src || post?.frontmatter?.image?.src || returnValue.image);
-    returnValue.image_alt = String(post?.image?.alt || post?.frontmatter?.image?.alt || returnValue.image_alt);
+    returnValue.imageAltText = String(post?.image?.alt || post?.frontmatter?.image?.alt || returnValue.imageAltText);
     returnValue.description = String(post?.description || post?.frontmatter?.description || returnValue.description);
-    returnValue.url = String(post?.url || post?.frontmatter?.url || returnValue.url);
+    returnValue.URL = String(post?.url || post?.frontmatter?.url || returnValue.URL);
 
     return returnValue;
 }
